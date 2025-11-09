d159116652f25d21186048c595669814b587afce
Tara <olasojinifemi67@gmail.com>
2025-11-09 07:05:07 +0100

    feat: add MainPage component and update Header with new functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Header from "./Component/Header";
+import MainPage from "./Component/MainPage";
 
 function App(params) {
    const [isDark, setIsDark] = useState(true);
@@ -7,6 +8,7 @@ function App(params) {
       <>
          <div className={`${isDark ? 'dark' : ''} h-screen flex flex-col`}>
              <Header/>
+             <MainPage/>
          </div>
       </>
    )
---
+++ src/Component/MainPage.jsx
@@ -0,0 +1,10 @@
+import React, {useState} from "react";
+
+function MainPage(params) {
+    
+    return(
+        <>
+        </>
+    )
+}
+export default MainPage;
